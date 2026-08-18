c00e7284b91cc3710d25fc47f967e35d7e84d101
esosapossibleboy-bit <esosapossibleboy@gmail.com>
2026-08-18 19:51:39 +0200

    Build a JavaScript Trivia Bot Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
    User Stories:
    
    You should log "Hello! I'm your coding fun fact guide!" to the console as a greeting message to the user.
    You should create three variables: botName, botLocation, and favoriteLanguage, that store the bot's name, where it's from, and its favorite programming language, respectively.
    You should log "My name is (botName) and I live on (botLocation)." to the console.
    You should log "My favorite programming language is (favoriteLanguage)." to the console.
    You should use let to create a codingFact variable and assign it a string that is a fun fact about the bot's favorite programming language, using the favoriteLanguage variable.
    You should log the codingFact to the console.
    You should reassign the codingFact variable to a new fact about the bot's favorite programming language using the favoriteLanguage variable again.
    You should log the codingFact to the console again.
    You should reassign the codingFact variable to a third fact about the bot's favorite programming language using the favoriteLanguage variable again.
    You should log the codingFact to the console a third time.
    You should log "It was fun sharing these facts with you. Goodbye! - (botName) from (botLocation)." to the console as a farewell statement from the bot.

--- test2.js
+++ test2.js
@@ -1,1 +1,21 @@
-console.log("Hey, I'm writing JavaScript!");
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "cheat";
+let botLocation = "computer space";
+let favoriteLanguage = "beeps";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "My favorite programming language is " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
+codingFact = "I started learning" + favoriteLanguage + " around 3 months ago.";
+console.log(codingFact);
+
+codingFact = "I like " + favoriteLanguage + "because it is the most simple language ever.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
